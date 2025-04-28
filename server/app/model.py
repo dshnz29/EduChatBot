c5171390834f2949bb79a056abd123635c8d5a93
H.M.D.B. Bandara <dshnz29@gmail.com>
2025-04-28 23:01:33 +0530

    //

--- server/app/model.py
+++
@@ -1,8 +0,0 @@
-from .db import mysql
-
-def create_user(name, email, age, preferred_field):
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO User (name, email, age, preferred_field) VALUES (%s, %s, %s, %s)",
-                (name, email, age, preferred_field))
-    mysql.connection.commit()
-    cur.close()
