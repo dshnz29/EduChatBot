78ff782514778536c8c7ab178ad9a844893ae05f
H.M.D.B. Bandara <130538167+dshnz29@users.noreply.github.com>
2025-04-28 23:05:58 +0530

    Delete ChatBot directory

--- ChatBot/server/app/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    DEBUG = True
-    SECRET_KEY = '0000'  # You can put any random string
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/chatbot'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- ChatBot/server/app/utils.py
+++
@@ -1,8 +0,0 @@
-def simple_ml_response(user_message):
-    # Temporary dummy reply (later you can add Gemini API or custom ML model)
-    if "engineer" in user_message.lower():
-        return "Engineering is a great field! Are you interested in Software, Civil, or Mechanical?"
-    elif "business" in user_message.lower():
-        return "Business careers are diverse! Would you like management, marketing, or finance?"
-    else:
-        return "Tell me more about your interests so I can guide you!"
